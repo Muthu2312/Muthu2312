221153aa94da7cee14d213a337f3d5fef038208f
Mutharasan_Manoharan <98216181+Muthu2312@users.noreply.github.com>
2023-02-26 01:35:22 +0530

    late1
    without prediction

--- stock_prediction.py
+++ stock_prediction.py
@@ -137,3 +137,4 @@ if selected=='Graphical Analysis':
         fig3 = px.box(data1, x=data1['Date'].dt.year, y='Close', points='all', title='Closing Prices by Year')
         st.plotly_chart(fig3,renderer='webgl')
          
+#
