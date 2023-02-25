7fd3b149e7628da81f8aece3fc822c5f3006f28a
Mutharasan_Manoharan <98216181+Muthu2312@users.noreply.github.com>
2023-02-26 02:49:03 +0530

    Update stock_prediction.py

--- stock_prediction.py
+++ stock_prediction.py
@@ -136,7 +136,7 @@ if selected=='Graphical Analyser':
         fig3 = px.box(data1, x=data1['Date'].dt.year, y='Close', points='all', title='Closing Prices by Year')
         st.plotly_chart(fig3,renderer='webgl')
         
-    break
+break
 if selected=='Predictions':
         df1=data1.reset_index()['Close']
         st.write(df1)
