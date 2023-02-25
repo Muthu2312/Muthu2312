41fa0b30504d93ab72bd8249094dee9c6e1c0d43
Mutharasan_Manoharan <98216181+Muthu2312@users.noreply.github.com>
2023-02-26 02:53:08 +0530

    Update stock_prediction.py

--- stock_prediction.py
+++ stock_prediction.py
@@ -137,7 +137,7 @@ if selected=='Graphical Analyser':
         st.plotly_chart(fig3,renderer='webgl')
         
 else:
-        pass
+        st.write("Enter correct stock name")
 if selected=='Predictions':
         df1=data1.reset_index()['Close']
         st.write(df1)
